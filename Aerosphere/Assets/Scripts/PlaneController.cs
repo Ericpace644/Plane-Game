2507287422f41d201b5113865d6dbeb1cc0c4485
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2023-05-30 10:54:15 -0700

    plane controller

--- Aerosphere/Assets/Scripts/PlaneController.cs
+++ Aerosphere/Assets/Scripts/PlaneController.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlaneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Plane Stats")]
+    [Tooltip("How much the throttle ramps up or down")]
+    public float throttleIncrement = 0.1f;
+    [Tooltip("Maximum engine thrust at 100% throttle")]
+    public float maxthrust = 200f;
+    [Tooltip("How responsive the plane is when rolling, pitching or yawing")]
+    public float responsiveness = 10f;
+
+    private float throttle;
+    private float roll;
+    private float pitch;
+    private float yaw;
+
+    private float responseModifier
     {
-        
+        get
+        {
+            return (rb.mass / 10f) * responsiveness;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void HandleInputs()
+    {
+        roll = Input.GetAxis("Roll");
+        pitch = Input.GetAxis("Pitch");
+        yaw = Input.GetAxis("Yaw");
+
+        if (Input.GetKey(KeyCode.Space)) throttle += throttleIncrement;
+        else if (Input.GetKey(KeyCode.LeftControl)) throttle -= throttleIncrement;
+        throttle = Mathf.Clamp(throttle, 1f, 100f);
+    }
+
+    private void Update()
+    {
+        HandleInputs();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * maxthrust * throttle);
+        rb.AddTorque(transform.up * yaw * responseModifier);
+        rb.AddTorque(transform.right * pitch * responseModifier);
+        rb.AddTorque(transform.forward * roll * responseModifier);
     }
 }
