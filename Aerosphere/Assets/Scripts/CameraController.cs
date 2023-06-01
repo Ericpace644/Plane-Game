cb0ea58d671284139bbda17c9c55c9ff6386e7c4
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2023-05-31 23:51:07 -0700

    ugh unity

--- Aerosphere/Assets/Scripts/CameraController.cs
+++ Aerosphere/Assets/Scripts/CameraController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform[] povs;
+    [SerializeField] float speed;
+
+    private int index = 0;
+    private Vector3 target;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P)) index += 1;
+        if (index >= 4) index = 0;
+
+        target = povs[index].position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.forward = povs[index].forward;
+
     }
 }
